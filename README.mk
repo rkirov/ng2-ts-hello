b9b5673a6db9c01dedd341c6a572c9fb69ad09ed
Rado Kirov <radokirov@google.com>
2015-04-21 22:01:00 -0700

    initial commit

---
+++ README.mk
@@ -0,0 +1,6 @@
+# Simple Angular 2 app writen in TS
+
+## Compile
+Using a TS compiler at head.
+
+tsc main.ts -m system -t ES5 --emitDecoratorMetadata
---
+++ main.ts
@@ -0,0 +1,33 @@
+/// <refence path="typings/angular2/angular2.d.ts" />
+
+import {Component, bootstrap, View, For} from "angular2/angular2";
+
+@Component({
+    selector: 'cmp'
+})
+@View({
+    template: `
+    <h1>Not todo</h1>
+    <ul *for="#noTodo of noTodos">
+      <li>
+        {{ noTodo }}
+      </li>
+    </ul>
+    <input #textbox>
+    <button (click)="addNoTodo(textbox.value)">Add</button>
+    `,
+    directives: [For]
+})
+class MyCmp {
+    noTodos: string[];
+
+    constructor() {
+        this.noTodos = ["hello"];
+    }
+
+    addNoTodo(noTodo: string) {
+        this.noTodos.push(noTodo);
+    }
+}
+
+bootstrap(MyCmp);
