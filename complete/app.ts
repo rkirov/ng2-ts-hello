9006dc292c37359a8d5ca6aee261ba04b5f3d149
Alex Eagle <alexeagle@google.com>
2015-04-27 13:12:38 -0700

    Simplify example to work for a quickstart

---
+++ complete/app.ts
@@ -0,0 +1,19 @@
+/// <reference path="typings/angular2/angular2.d.ts" />
+
+import {Component, bootstrap, View} from "angular2/angular2";
+
+@Component({
+    selector: 'my-app'
+})
+@View({
+    template: '<h1>Hello {{ name }}</h1>'
+})
+class MyAppComponent {
+    name: string;
+
+    constructor() {
+        this.name = 'Alice';
+    }
+}
+
+bootstrap(MyAppComponent);
